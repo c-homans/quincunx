53f497a204080a936d6a5f220c1d2adca36308fa
H <h@littleghost.local>
2021-06-21 13:19:53 -0500

    minor cleanup

--- quincunx.py
+++ quincunx.py
@@ -4,37 +4,33 @@ from os import system
 import sys
 from termcolor import colored, cprint
 
-N = 1000
-n = 15
-middle = n
+N = 15000
+n = 40
 bins = [0]*n*2
 
 quincunx = [' '*2*n]*(n)
 for k in range(0, n):
     d = k+1
-    quincunx[k] = quincunx[k][:middle-d] + '/\\'*(d) + quincunx[k][middle+d:] 
+    quincunx[k] = quincunx[k][:n-d] + '/\\'*(d) + quincunx[k][n+d:] 
 
 output = [' '*(n*2)]
 for ball in range(N):
     to_print = quincunx[:]
     offset = 0
     for k in range(n):
-        #to_print = ' '*2*n
         if choice((True, False)):
-            to_print[k] = to_print[k][:middle+offset] + colored('\\', 'blue') + to_print[k][middle+offset+1:]
+            to_print[k] = to_print[k][:n+offset] + colored('\\', 'blue') + to_print[k][n+offset+1:]
             offset += 1
         else:
-            to_print[k] = to_print[k][:middle+offset-1] + colored('/', 'blue') + to_print[k][middle+offset:]
+            to_print[k] = to_print[k][:n+offset-1] + colored('/', 'blue') + to_print[k][n+offset:]
             offset -= 1
-        #sleep(0.01)
-        system('clear')
-        print('\n'.join(to_print))
         if k == n-1:
-            print(' '*(middle+offset)+colored('X', 'blue'))
-        else:
-            print(' ')
-        print(colored('\n'.join(output), 'blue'))
-    bins[middle+offset] += 1
+            system('clear')
+            print('\n'.join(to_print))
+            print(' '*(n+offset)+colored('X', 'blue'))
+            print(colored('\n'.join(output), 'blue'))
+    sleep(0.1)
+    bins[n+offset] += 1
     for i, count in enumerate(bins):
         tens = count // 10
         ones = count % 10
@@ -46,5 +42,3 @@ for ball in range(N):
                             + output[row][i+1:])
         output[tens] =   output[tens][:i] + (str(ones) if ones != 0 else ' ') \
             + output[tens][i+1:]
-    #sleep(0.05)
-    #print(list(enumerate(bins)))
---
+++ test.py
@@ -0,0 +1,5 @@
+from time import sleep
+#while(True):
+for i in range(20):
+    print('test')
+    sleep(1)
