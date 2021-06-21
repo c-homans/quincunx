d564a3b888788a65986b0171b59dae0bd0326c5c
H <h@littleghost.local>
2021-06-21 15:13:52 -0500

    more robust

--- quincunx.py
+++ quincunx.py
@@ -3,48 +3,55 @@ from time import sleep
 from os import system
 from termcolor import colored, cprint
 
-N = 1500
+N = 300
 n = 20
 bins = [0]*n*2
 
-# TODO: dynamic color control
+# TODO: command-line control
 move_color = None #'blue'
-quincunx_color = 'blue'
+quincunx_color = None #None
 output_color = None #'blue'
 
-move_time = 0.0 #0.005
-ball_time = 0.02 #0.1
+move_time = 0 #0.005
+ball_time = 0.05 #0.1
 
 scale = 'hundreds'
 
-right_move_icon = colored('\\', move_color) #\\
-left_move_icon = colored('/', move_color) #/
+right_move_icon = colored('#', move_color) #\\
+left_move_icon = colored('#', move_color) #/
 
-quincunx = [' '*2*n]*(n)
+def format(string, color):
+    if color:
+        return colored(string, color)
+    else:
+        return string
+
+raw_quincunx = [' '*2*n]*(n)
 for k in range(0, n):
     d = k+1
-    quincunx[k] = quincunx[k][:n-d] + '/\\'*(d) + quincunx[k][n+d:] 
-#quincunx = list(map(lambda row : colored(row, quincunx_color), quincunx))
+    raw_quincunx[k] = raw_quincunx[k][:n-d] + '/\\'*(d) + raw_quincunx[k][n+d:] 
 output = [' '*(n*2)]
-for ball in range(N):
-    to_print = quincunx[:]
-    offset = 0
+for ball in range(1, N+1):
+    pos = 0
+    quincunx = list(map(lambda s : colored(s, quincunx_color), raw_quincunx))
     for k in range(n):
         sleep(move_time)
+        #print(to_print)
         if choice((True, False)):
-            to_print[k] = colored(to_print[k][:n+offset], quincunx_color) + right_move_icon + colored(to_print[k][n+offset+1:], quincunx_color)
-            offset += 1
+            quincunx[k] = format(raw_quincunx[k][:n+pos], quincunx_color) + right_move_icon + format(raw_quincunx[k][n+pos+1:], quincunx_color)
+            pos += 1
         else:
-            to_print[k] = colored(to_print[k][:n+offset-1], quincunx_color) + left_move_icon + colored(to_print[k][n+offset:], quincunx_color)
-            offset -= 1
+            quincunx[k] = format(raw_quincunx[k][:n+pos-1], quincunx_color) + left_move_icon + format(raw_quincunx[k][n+pos:], quincunx_color)
+            pos -= 1
         if k == n-1 or move_time > 0:
             system('clear')
+            to_print = quincunx[:]
             to_print.insert(0, f'Move time: {move_time}. Rest time: {ball_time}. Ball {ball} of {N}.')
-            to_print.append(' '*(n+offset)+colored('X', output_color))
+            to_print.append(' '*(n+pos)+colored('X', output_color))
             to_print.append(colored('\n'.join(output), output_color))
             print('\n'.join(to_print))
     sleep(ball_time)
-    bins[n+offset] += 1
+    bins[n+pos] += 1
     for i, count in enumerate(bins):
         hunds = count // 100
         tens_mod_100 = (count % 100) // 10
--- test.py
+++
@@ -1,5 +0,0 @@
-from time import sleep
-#while(True):
-for i in range(20):
-    print('test')
-    sleep(1)
