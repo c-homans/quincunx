f1e8c9f92a3b2dc58208c4d2fd0d39981fe1adce
chomans <charles@homans.io>
2021-06-24 20:29:07 -0500

    arg descriptions

--- quincunx.py
+++ quincunx.py
@@ -9,23 +9,23 @@ colorama.init()  # helps print termcolor on non-unix systems
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Configuration for the quincunx.')
-    parser.add_argument('-n', default=30)
-    parser.add_argument('-B', default=500)
-    parser.add_argument('-scale', default='tens')
-    parser.add_argument('-move', default=0)
-    parser.add_argument('-rest', default=0.2)
-    parser.add_argument('-move_color', default=None)
-    parser.add_argument('-quincunx_color', default=None)
-    parser.add_argument('-output_color', default=None)
-    parser.add_argument('-lmove', default='#')
-    parser.add_argument('-rmove', default='#')
+    parser.add_argument('-n', default=30, help='Number of experiments.')
+    parser.add_argument('-B', default=500, help='Number of iterations (or \'beans\' for the bean machine).')
+    parser.add_argument('-scale', default='tens', help='Number of experiments for a full tally row in output -- \'tens\' or \'hundreds\'')
+    parser.add_argument('-move', default=0, help='Time to wait between each experiment.')
+    parser.add_argument('-rest', default=0.2, help='Time to wait between each iteration.')
+    parser.add_argument('-move_color', default=None, help='Color to indicate the path each \'bean\' takes.')
+    parser.add_argument('-quincunx_color', default=None, help='Color of the quincunx representation.')
+    parser.add_argument('-output_color', default=None, help='Color of the output distribution.')
+    parser.add_argument('-lmove', default='#', help='Symbol for experiments which result in a left movemnt.')
+    parser.add_argument('-rmove', default='#', help='Symbol for experiments which result in a right movement.')
     return parser.parse_args()
 
 clear = '\033c'
 def draw_frame():
     to_print = qc[:]
     to_print.insert(
-        0, f'Move time: {move_time}. Rest time: {ball_time}. Ball {ball} of {N}.')
+        0, f'Move time: {move_time}. Rest time: {ball_time}. Bean {ball} of {N}.')
     to_print.append(' '*(n+pos)+(colored('+', output_color)) if ball else '\n')
     to_print.append(colored('\n'.join(output), output_color))
     to_print.insert(0, clear)
@@ -44,10 +44,14 @@ def update_quincunx(k, pos):
     sleep(move_time)
     #print(to_print)
     if choice((True, False)):
-        qc[k] = colored(raw_qc[k][:n+pos], quincunx_color) + right_move_icon + colored(raw_qc[k][n+pos+1:], quincunx_color)
+        qc[k] =  ( colored(raw_qc[k][:n+pos], quincunx_color)
+                 + right_move_icon 
+                 + colored(raw_qc[k][n+pos+1:], quincunx_color) )
         pos += 1
     else:
-        qc[k] = colored(raw_qc[k][:n+pos-1], quincunx_color) + left_move_icon + colored(raw_qc[k][n+pos:], quincunx_color)
+        qc[k] =  (colored(raw_qc[k][:n+pos-1], quincunx_color)
+                 + left_move_icon
+                 + colored(raw_qc[k][n+pos:], quincunx_color) )
         pos -= 1
     if k == n-1 or (move_time > 0 and k % 3 == 0):
         draw_frame()
@@ -64,7 +68,7 @@ def update_output_bin(i, count):
     for row in range(big):
         output[row] = ( output[row][:i]
                       + 'X'
-                      + output[row][i+1:])
+                      + output[row][i+1:] )
     output[big] = ( output[big][:i]
                   + (str(small) if count > 0 else ' ')
                   + output[big][i+1:] )
